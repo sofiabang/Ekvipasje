a385811c5735f137509c2fefae50a93d8673f163
sofiabang <karolinasbang@gmail.com>
2023-05-26 15:08:34 +0200

    Tidy up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.getElementById('login-link').addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent default link behavior
+  event.preventDefault(); 
 
-  window.location.href = 'login.html'; // Redirect to login.html
+  window.location.href = 'login.html'; 
 });
 
 const loginForm = document.querySelector('.login-form');
@@ -9,32 +9,32 @@ const usernameInput = document.querySelector('#username');
 const passwordInput = document.querySelector('#password');
 
 document.getElementById('login-button').addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent form submission
+  event.preventDefault(); 
 
-  // Perform login logic here
+  
 
-  window.location.href = 'hjem.html'; // Redirect to hjem.html
+  window.location.href = 'hjem.html'; 
 });
 
 document.getElementById('search-button').addEventListener('click', function() {
   var searchTerm = document.getElementById('search-input').value;
-  // Perform search operation with the searchTerm value
+  
   console.log('Search term: ' + searchTerm);
 });
 
 document.getElementById('login-button').addEventListener('click', function() {
-  window.location.href = 'hjem.html'; // Redirect to hjem.html
+  window.location.href = 'hjem.html'; 
 });
 
 document.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     var activeElement = document.activeElement;
     if (activeElement === usernameInput || activeElement === passwordInput) {
-      event.preventDefault(); // Prevent form submission on Enter key
+      event.preventDefault(); 
 
-      // Perform any login logic here if needed
+      
 
-      window.location.href = 'hjem.html'; // Redirect to hjem.html
+      window.location.href = 'hjem.html'; 
     }
   }
 });
