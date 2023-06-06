ab0f38786e6e4c82afc255128e5b28b01a40eba7
sofiabang <karolinasbang@gmail.com>
2023-06-06 21:21:29 +0200

    cleaned up some code

--- js/script.js
+++ js/script.js
@@ -1,57 +1,53 @@
 // Log in
 
-const loginForm = document.querySelector('.login-form');
-const usernameInput = document.querySelector('#username');
-const passwordInput = document.querySelector('#password');
+const loginForm = document.querySelector(".login-form");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
 
-document.getElementById('login-button').addEventListener('click', function() {
-  window.location.href = 'hjem.html'; 
+document.getElementById("login-button").addEventListener("click", function () {
+  window.location.href = "hjem.html";
 });
 
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
     var activeElement = document.activeElement;
     if (activeElement === usernameInput || activeElement === passwordInput) {
-      event.preventDefault(); 
+      event.preventDefault();
 
-      
-
-      window.location.href = 'hjem.html'; 
+      window.location.href = "hjem.html";
     }
   }
 });
 
-
 // HJEM
 
-document.getElementById('search-button').addEventListener('click', function() {
-  var searchTerm = document.getElementById('search-input').value;
-  
-  console.log('Search term: ' + searchTerm);
+document.getElementById("search-button").addEventListener("click", function () {
+  var searchTerm = document.getElementById("search-input").value;
+
+  console.log("Search term: " + searchTerm);
 });
 
-const heartButton = document.getElementById('my-heart-button');
+const heartButton = document.getElementById("my-heart-button");
 
-heartButton.addEventListener('click', toggleClickedState);
+heartButton.addEventListener("click", toggleClickedState);
 
-const menuItems = document.querySelectorAll('.menu li');
-
+const menuItems = document.querySelectorAll(".menu li");
 
 menuItems.forEach((menuItem) => {
-  const subMenu = menuItem.querySelector('.sub-menu');
+  const subMenu = menuItem.querySelector(".sub-menu");
 
-  menuItem.addEventListener('mouseenter', () => {
-    subMenu.style.display = 'block';
+  menuItem.addEventListener("mouseenter", () => {
+    subMenu.style.display = "block";
   });
 
-  menuItem.addEventListener('mouseleave', () => {
-    subMenu.style.display = 'none';
+  menuItem.addEventListener("mouseleave", () => {
+    subMenu.style.display = "none";
   });
 });
 
 // Hjem
 
 function toggleLikeButton() {
-  var button = document.querySelector('.card-heart-button');
-  button.classList.toggle('liked');
+  var button = document.querySelector(".card-heart-button");
+  button.classList.toggle("liked");
 }
